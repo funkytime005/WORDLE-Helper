67feb9f413aa1699c55b1628db4488796ffd1a2b
funky_time_ <kkuriata32@gmail.com>
2026-02-09 14:24:59 -0800

    Styles for buttons and text edit
    Holds variables for different PyQt5 button styles.

---
+++ styles.py
@@ -0,0 +1,45 @@
+
+button_default = """
+    QPushButton {
+        font-size: 30pt;
+        background-color: #FFFFFF; 
+        color: black;
+        border: 2px solid #AAAAAA;
+        border-radius: 5px;
+    }
+"""
+
+button_gray = """
+    QPushButton {
+        font-size: 30pt;
+        background-color: #787C7E;
+        color: white;
+    }
+"""
+
+button_green = """
+    QPushButton {
+        font-size: 30pt;
+        background-color: #6AAA64;
+        color: white;
+    }
+"""
+
+button_yellow = """
+    QPushButton {
+        font-size: 30pt;
+        background-color: #C9B458;
+        color: white;
+    }
+"""
+
+word_box = """
+    QTextEdit {
+        font-size: 10pt;
+        padding: 20px;
+        border: 2px solid #AAAAAA;
+        border-radius: 5px;
+        margin-top: 25px;
+        margin-side: 25px;
+    }
+"""
